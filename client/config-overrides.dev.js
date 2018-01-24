349bb9fb836387d57430b25073095c58287e08d2
cdiezmoran <cdiezmoran@gmail.com>
2018-01-24 00:25:21 -0600

    Add webpack support for .graphql files

--- client/config-overrides.dev.js
+++ client/config-overrides.dev.js
@@ -1,25 +1,33 @@
 module.exports = (config) => {
   // Add the SASS loader second-to-last
   // (last one must remain as the "file-loader")
-  const loaderList = config.module.rules[1].oneOf;
-  loaderList.splice(loaderList.length - 1, 0, {
-    test: /^((?!\.global).)*\.scss$/,
-    use: [
-      {
-        loader: 'style-loader'
-      },
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          sourceMap: true,
-          importLoaders: 1,
-          localIdentName: '[name]__[local]__[hash:base64:5]',
+  const options = [
+    {
+      test: /^((?!\.global).)*\.scss$/,
+      use: [
+        {
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            sourceMap: true,
+            importLoaders: 1,
+            localIdentName: '[name]__[local]__[hash:base64:5]',
+          }
+        },
+        {
+          loader: 'sass-loader'
         }
-      },
-      {
-        loader: 'sass-loader'
-      }
-    ]
-  });
+      ]
+    },
+    {
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    }
+  ];
+  const loaderList = config.module.rules[1].oneOf;
+  loaderList.splice(loaderList.length - 1, 0, ...options);
 };
