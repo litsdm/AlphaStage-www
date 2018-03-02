f53b25668250850e0dbd1299ac425619167094b8
cdiezmoran <cdiezmoran@gmail.com>
2018-03-01 23:46:49 -0600

    Add testerId when creating test document

--- server/src/resolvers/tests.js
+++ server/src/resolvers/tests.js
@@ -1,13 +1,20 @@
 import dataBase from '../db';
-import { prepare } from './index';
+import { prepare, getObjectId } from './index';
 
 const db = dataBase.get();
 
 const Tests = db.collection('tests');
+const TestingSessions = db.collection('testingSessions');
 
 const tests = {
   Mutation: {
     createTest: async (root, { test }) => {
+      await TestingSessions.findAndModify(
+        { _id: getObjectId(test.testingSessionId) },
+        { _id: 1 },
+        { $push: { testerIds: test.testerId } },
+        { upsert: true }
+      );
       const res = await Tests.insert(test);
       return prepare(await Tests.findOne({ _id: res.insertedIds[0] }));
     }
