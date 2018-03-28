0b070c6f6e9912c046ebf5642db1a5822186080a
cdiezmoran <cdiezmoran@gmail.com>
2018-03-28 17:20:33 -0600

    Upload to correct s3 folder

--- server/server.js
+++ server/server.js
@@ -152,9 +152,10 @@ export const server = async () => {
       const s3 = new aws.S3();
       const fileName = req.query['file-name'];
       const fileType = req.query['file-type'];
+      const folderName = req.query['folder-name'];
       const s3Params = {
         Bucket: S3_BUCKET,
-        Key: fileName,
+        Key: `${folderName}/${fileName}`,
         Expires: 60,
         ContentType: fileType,
         ACL: 'public-read'
@@ -167,7 +168,7 @@ export const server = async () => {
         }
         const returnData = {
           signedRequest: data,
-          url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+          url: `https://${S3_BUCKET}.s3.amazonaws.com/${folderName}/${fileName}`
         };
         res.write(JSON.stringify(returnData));
         res.end();
