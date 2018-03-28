1bd47e1cc82f200ee66dfc130b57b5f0de5f72b5
cdiezmoran <cdiezmoran@gmail.com>
2018-03-27 20:34:14 -0600

    Fix mlab connection

--- server/src/db.js
+++ server/src/db.js
@@ -2,9 +2,11 @@ import { MongoClient } from 'mongodb';
 
 require('dotenv').config();
 
-const dbName = process.env.AS_ENV === 'production' ? 'alphastage-prod' : 'alphastage-dev';
+const dbName = process.env.AS_ENV === 'production'
+  ? 'ds125479.mlab.com:25479/alphastage-prod'
+  : 'ds151963.mlab.com:51963/alphastage-dev';
 
-const MONGO_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds151963.mlab.com:51963/${dbName}`;
+const MONGO_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${dbName}`;
 
 let _db;
 
