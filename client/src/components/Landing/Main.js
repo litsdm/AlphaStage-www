b8645728476662f4adcbb144b7ac6534ec9d1b3b
cdiezmoran <cdiezmoran@gmail.com>
2018-03-29 15:09:59 -0600

    Add testing info section

--- client/src/components/Landing/Main.js
+++ client/src/components/Landing/Main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
+import TestingInfo from './TestingInfo';
 
 const Main = ({ submitUser }) => (
   <div>
     <Header submitUser={submitUser} />
+    <TestingInfo />
   </div>
 );
 
